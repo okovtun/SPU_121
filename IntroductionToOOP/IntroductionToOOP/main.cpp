fe84732f68aa7f02c5510ca88153b6e299ebd1aa
Clayman <Clayman@DESKTOP-DM8E77U>
2021-10-16 15:05:34 +0300

    Operators overloading day 1

--- IntroductionToOOP/IntroductionToOOP/main.cpp
+++ IntroductionToOOP/IntroductionToOOP/main.cpp
@@ -1,5 +1,8 @@
 ﻿#include<iostream>
-using namespace std;
+//using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
 
 #define tab "\t"
 
@@ -19,10 +22,14 @@ public:
 	}
 	void set_x(double x)
 	{
+		if (x < 0)x = 0;
+		if (x > 1000)x = 1000;
 		this->x = x;
 	}
 	void set_y(double y)
 	{
+		if (y < 0)y = 0;
+		if (y > 1000)y = 1000;
 		this->y = y;
 	}
 
@@ -38,8 +45,9 @@ public:
 		this->y = y;
 		cout << "Constructor:\t\t" << this << endl;
 	}
-	Point(const Point& other)
+	Point(const Point& other)	//Конструктор копирования копирует объект
 	{
+		//а именно, СОЗДАВАЕМЫЙ ОБЪЕКТ делает точной копией СУЩЕСТВУЮЩЕГО ОБЪЕКТА.
 		this->x = other.x;
 		this->y = other.y;
 		cout << "CopyConstructor:\t" << this << endl;
@@ -47,6 +55,37 @@ public:
 	~Point()
 	{
 		cout << "Destructor:\t\t" << this << endl;
+	}
+
+	//						Operators:
+	Point& operator=(const Point& other)
+	{
+		this->x = other.x;
+		this->y = other.y;
+		cout << "CopyAssignment:\t\t" << this << endl;
+		return *this;
+	}
+	Point& operator+=(const Point& other)
+	{
+		this->x += other.x;
+		this->y += other.y;
+		return *this;
+	}
+
+	//					Increment/Decrement
+	Point& operator++()		//Prefix increment
+	{
+		this->x++;
+		this->y++;
+		return *this;
+	}
+	Point operator++(int)	//Suffix increment
+	{
+		Point old = *this;
+		this->x++;
+		this->y++;
+		return old;
+		true || false;//bool
 	}
 
 	//						Methods:
@@ -64,6 +103,27 @@ public:
 	}
 };
 
+Point operator+(const Point& left, const Point& right)
+{
+	Point res;
+	res.set_x(left.get_x() + right.get_x());
+	res.set_y(left.get_y() + right.get_y());
+	return res;
+}
+Point operator-(const Point& left, const Point& right)
+{
+	Point res(left.get_x() - right.get_x(), left.get_y() - right.get_y());
+	return res;
+}
+
+bool operator==(const Point& left, const Point& right)
+{
+	if (left.get_x() == right.get_x() && left.get_y() == right.get_y())
+		return true;
+	else
+		return false;
+}
+
 double distance(const Point& A, const Point& B)
 {
 	/*double x_distance = A.get_x() - B.get_x();
@@ -74,9 +134,14 @@ double distance(const Point& A, const Point& B)
 	return pow(pow(A.get_x() - B.get_x(), 2) + pow(A.get_y() - B.get_y(), 2), .5);
 }
 
+#define delimiter "\n-----------------------------------\n"
+
 //#define STRUCT_POINT
-#define DISTANCE
+//#define DISTANCE
 //#define CONSTRUCTORS_CHECK
+//#define ASSIGNMENT_CHECK
+//#define OPERATORS
+//#define ARITHMENTICAL_OPERATORS
 
 void main()
 {
@@ -100,14 +165,15 @@ void main()
 	//Seems like we explicitly call default constructor for int
 
 #ifdef DISTANCE
-	Point A;
+	Point A;	//Default constructor
 	A.set_x(2);
 	A.set_y(3);
 	cout << A.get_x() << "\t" << A.get_y() << endl;
 	//get-методы
-	Point B;
+	Point B;	//Default constructor
 	B.set_x(4);
 	B.set_y(5);
+	B.print();
 	//Метод distance - возвращает расстояние до указанной точки.
 	cout << "\n-----------------------------------\n";
 	cout << A.distance(B) << endl;
@@ -139,8 +205,61 @@ cout << a << endl;*/
 	Point C = B;	//Copy constructor
 	C.print();
 	Point D(C);		//Copy constructor
+	Point E;	//Default constructor
+	E = C;	//Copy assignment
+	E.print();
+	A = B;
+	A.print();
 #endif // CONSTRUCTORS_CHECK
 
+#ifdef ASSIGNMENT_CHECK
+	int a, b, c;
+	a = b = c = 0;
+	Point A, B, C;	//Default constructor для всех трех точек
+	cout << delimiter << endl;
+	A = B = C = Point(15, 18);
+	cout << delimiter << endl;
+	A.print();
+	B.print();
+	C.print();
+#endif // ASSIGNMENT_CHECK
+
+#ifdef OPERATORS
+	int a = 2;
+	int b = 3;
+	a + b;	//Простое выражение
+	a;	//Простейшее выражение
+	;	//Пустое выражение
+	int c = a + b;	//Сложное (составное) выражение
+	a += b;
+#endif // OPERATORS
+
+#ifdef ARITHMENTICAL_OPERATORS
+	Point A(2, 3);
+	Point B(3, 4);
+	A += B;
+	A.print();
+	Point C = A + B;
+	C.print();
+	C = A - B;
+	C.print();
+	Point D = C++;
+	C.print();
+	D.print();
+#endif // ARITHMENTICAL_OPERATORS
+
+	Point A(2, 3);
+	//Point B(3, 4);
+	Point B = A;
+	//B++;
+	if (A == ++B)
+	{
+		cout << "Точки равны" << endl;
+	}
+	else
+	{
+		cout << "Точки разные" << endl;
+	}
 }
 
 /*
@@ -179,5 +298,29 @@ Point();	//Конструктор класса Point
 1. Конструктор по умолчанию (Default constructor) - это конструктор, который
    может быть вызван без параметров.
 2. Конструктор копирования  (CopyConstructor) - это конструктор, который копирует объект.
+---------------------------------------------------------------
+*/
+
+/*
+---------------------------------------------------------------
+					OPERATORS OVERLOADING
+1. Перегрузить можно лишь существующие операторы.
+	+  - перегружается;
+	*  - перегружается;
+	++ - перегружается;
+	** - НЕ перегружается;
+2. Не все существующие операторы можно перегрузить.
+   Не перегружаются:
+	?: - Тернарный оператор
+	:: - Scope operator - Оператор разрешения видимости;
+	.  - Point operator - Оператор прямого доступа;
+	.*
+	#
+	##
+3. Перегруженные операторы сохраняют приоритет;
+4. Переопределить поведение операторов
+   со встроенными типами данны невозможно.
+
+
 ---------------------------------------------------------------
 */
